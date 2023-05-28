4576f9fa39b9338c4d28b4790b84dfb214865172
kurizz <umozon63313@gmail.com>
2023-05-28 23:17:42 +0900

    fix custom user's plugings

---
+++ .config/nvim/lua/user/plugins/misc.lua
@@ -0,0 +1,6 @@
+return {
+    "folke/tokyonight.nvim",
+    "xiyaowong/transparent.nvim",
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+}
